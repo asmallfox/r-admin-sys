0bfa7682e0a10dd7aff916fc6b74be46c6f5618d
lzj <1257092503@qq.com>
2023-03-14 17:43:10 +0800

    recon: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,22 @@
-import type { RouterRaws } from './routes/types'
+// import type { RouterRaws } from './routes/types'
 
-const layoutRoutes: RouterRaws[] = []
+// const layoutRoutes: RouterRaws[] = []
 
-const modules = import.meta.glob('./modules/**.tsx', { eager: true })
-Object.keys(modules).forEach(key => {
-  const module = (modules[key] as any).default
-  const mod = Array.isArray(module) ? [...module] : [module]
-  layoutRoutes.push(...mod)
-})
+// const modules = import.meta.glob('./modules/**.tsx', { eager: true })
+// Object.keys(modules).forEach(key => {
+//   const module = (modules[key] as any).default
+//   const mod = Array.isArray(module) ? [...module] : [module]
+//   layoutRoutes.push(...mod)
+// })
 
-export {
-  layoutRoutes
-}
+// export {
+//   layoutRoutes
+// }
+
+
+import { basicRoutes } from './routes'
+import { useRoutes } from 'react-router-dom'
+
+const RouterElement = () => useRoutes(basicRoutes)
+
+export default RouterElement
--- src/router/routes/index.tsx
+++ src/router/routes/index.tsx
@@ -3,11 +3,13 @@ import { Navigate } from 'react-router-dom'
 
 import { PageEnum } from '@/enums/pageEnum'
 
+import Login from '@/views/login/login'
+
 const modules = import.meta.glob('./modules/**/*.tsx', { eager: true })
 const routeModuleList: RouterRaws[] = []
 
 Object.keys(modules).forEach(key => {
-  const module = (modules[key] as any).default
+  const module = modules[key].default
   const mod = Array.isArray(module) ? [...module] : [module]
   routeModuleList.push(...mod)
 })
@@ -18,3 +20,12 @@ export const RootRoute: RouterRaws = {
   path: '/',
   element: <Navigate to={PageEnum.BASE_HOME}/>
 }
+
+export const LoginRoute: RouterRaws = {
+  path: '/login',
+  element: <Login />,
+  meta: {
+    label: '登录'
+  }
+}
+export const basicRoutes = [RootRoute, LoginRoute, ...routeModuleList]
